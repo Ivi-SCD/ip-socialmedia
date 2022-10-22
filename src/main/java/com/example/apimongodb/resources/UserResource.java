b8b6cbbeeb34c759850c98ea0bd1f5944ee5d53b
Ivi <ivipnascimento@hotmail.com>
2022-10-22 18:16:16 -0300

    Using DTO pattern to return users

--- src/main/java/com/example/apimongodb/resources/UserResource.java
+++ src/main/java/com/example/apimongodb/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.apimongodb.domain.User;
+import com.example.apimongodb.dto.UserDTO;
 import com.example.apimongodb.services.UserService;
 
 @RestController
@@ -20,9 +21,10 @@ public class UserResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	//Or @GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> allUsers = userService.findAll();
+		List<UserDTO> allUsersDto = allUsers.stream().map(x -> new UserDTO(x)).toList();
 		
-		return ResponseEntity.ok().body(allUsers);
+		return ResponseEntity.ok().body(allUsersDto);
 	}
 }
