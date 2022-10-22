f51c49c21f77c631ed79b26cc9fd05bc7097dd8a
Ivi <ivipnascimento@hotmail.com>
2022-10-22 18:47:33 -0300

    Insert user with POST

--- src/main/java/com/example/apimongodb/resources/UserResource.java
+++ src/main/java/com/example/apimongodb/resources/UserResource.java
@@ -1,13 +1,16 @@
 package com.example.apimongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.apimongodb.domain.User;
 import com.example.apimongodb.dto.UserDTO;
@@ -22,7 +25,7 @@ public class UserResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	//Or @GetMapping
-	public ResponseEntity<List<UserDTO>> findAll() {
+	public ResponseEntity <List<UserDTO>> findAll() {
 		List<User> allUsers = userService.findAll();
 		List<UserDTO> allUsersDto = allUsers.stream().map(x -> new UserDTO(x)).toList();
 		
@@ -30,9 +33,20 @@ public class UserResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+	public ResponseEntity <UserDTO> findById(@PathVariable String id) {
 		User user = userService.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity <Void> insertUser(@RequestBody UserDTO userDto) {
+		User user = userService.fromDTO(userDto);
+		user = userService.insertUser(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/example/apimongodb/services/UserService.java
+++ src/main/java/com/example/apimongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.apimongodb.domain.User;
+import com.example.apimongodb.dto.UserDTO;
 import com.example.apimongodb.repositories.UserRepository;
 import com.example.apimongodb.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional <User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not founded: " + id));
+	}
+	
+	public User insertUser(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
