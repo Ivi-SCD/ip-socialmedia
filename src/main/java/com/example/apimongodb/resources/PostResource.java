e32c7bc6bba0ad876851910dd8b038139781cec6
Ivi <ivipnascimento@hotmail.com>
2022-10-24 10:06:15 -0300

    Simple request with query methods

--- src/main/java/com/example/apimongodb/resources/PostResource.java
+++ src/main/java/com/example/apimongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.example.apimongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.apimongodb.domain.Post;
+import com.example.apimongodb.resources.util.URL;
 import com.example.apimongodb.services.PostService;
 
 @RestController
@@ -20,6 +24,14 @@ public class PostResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity <Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
+		
+		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity <List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List <Post> post = postService.findByTitle(text);
 		
 		return ResponseEntity.ok().body(post);
 	}
